758e49e7cdd5d4c3982e1f9754c60803facc1847
Alfio Parisi <alfioparisi.93@gmail.com>
2017-05-02 17:04:58 +0100

    Containers are subscribed to the store

--- src/App.js
+++ src/App.js
@@ -4,48 +4,93 @@ import {initial, work, education, projects, background} from './panels';
 import {expand, shrink, show} from './actions';
 import store from './reducers';
 
-// Should be a mixed component.
-const Header = ({title, description}) => (
-  <header>
-    <div>
-      // Show the title field of the Redux state.
-      <h1>{title}</h1>
-      // If a panel is open, show the button.
-      {title !== 'ALFIO PARISI' &&
-      <button
-        // onClick dispatch an action to shrink any open panel.
-        onClick={init => store.dispatch(shrink(initial))}
-      >Close</button>}
-    </div>
-    // If no panel is opened, show the profile pic and the contacts.
-    {title === 'ALFIO PARISI' && <div>Img wannabe</div>}
-    <div><p>{description}</p></div>
-  </header>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: initial.title,
+      description: initial.description
+    };
+  }
 
-// Container component (?)
-const Work = ({expandWork}) => (
-  <section>
-    // If the panel is not expanded just show an 'h2'.
-    {!expandWork &&
-    <h2
-      // onClick dispatch an action to expand this panel.
-      onClick={panel => store.dispatch(expand(work))}
-    >Work</h2>}
-    // If the panel is expanded show the content of the corresponding object.
-    {expandWork &&
-    // For every item in the array, return a component.
-    work.jobs.map(job => (
-      <Job key={job.id}
-        title={job.title}
-        employer={job.employer}
-        dates={job.dates}
-        description={job.description}
-        location={job.location}
-      />
-    ))}
-  </section>
-);
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState({
+      title: store.getState().title,
+      description: store.getState().description
+    }));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const title = this.state.title;
+    const description = this.state.description;
+    return (
+      <header>
+        <div>
+          // Show the title field of the Redux state.
+          <h1>{title}</h1>
+          // If a panel is open, show the button.
+          {title !== 'ALFIO PARISI' &&
+          <button
+            // onClick dispatch an action to shrink any open panel.
+            onClick={init => store.dispatch(shrink(initial))}
+          >Close</button>}
+        </div>
+        // If no panel is opened, show the profile pic and the contacts.
+        {title === 'ALFIO PARISI' && <div>Img wannabe</div>}
+        <div><p>{description}</p></div>
+      </header>
+    );
+  }
+}
+
+class Work extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expandWork: false
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState({
+      expandWork: store.getState().expandWork
+    }));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const expandWork = this.state.expandWork;
+    return (
+      <section>
+        // If the panel is not expanded just show an 'h2'.
+        {!expandWork &&
+        <h2
+          // onClick dispatch an action to expand this panel.
+          onClick={panel => store.dispatch(expand(work))}
+        >Work</h2>}
+        // If the panel is expanded show the content of the corresponding object.
+        {expandWork &&
+        // For every item in the array, return a component.
+        work.jobs.map(job => (
+          <Job key={job.id}
+            title={job.title}
+            employer={job.employer}
+            dates={job.dates}
+            description={job.description}
+            location={job.location}
+          />
+        ))}
+      </section>
+    );
+  }
+}
 
 // Presentational component.
 // Show the single item of the job array.
@@ -65,31 +110,53 @@ const Job = ({title, employer, dates, description, location}) => (
   </div>
 );
 
-const Education = ({expandEducation}) => (
-  <section>
-    {!expandEducation &&
-    <h2
-      onClick={panel => store.dispatch(expand(education))}
-    >Education</h2>}
-    {expandEducation &&
-    education.schools.map(school => (
-      <School key={school.id}
-        name={school.name}
-        dates={school.dates}
-        location={school.location}
-      />
-    ))}
-    {expandEducation &&
-      education.onlineCourses.map(course => (
-        <Course key={course.id}
-          name={course.name}
-          title={course.title}
-          dates={course.dates}
-          url={course.url}
-        />
-    ))}
-  </section>
-);
+class Education extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expandEducation: false
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState({
+      expandEducation: store.getState().expandEducation
+    }));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const expandEducation = this.state.expandEducation;
+    return (
+      <section>
+        {!expandEducation &&
+        <h2
+          onClick={panel => store.dispatch(expand(education))}
+        >Education</h2>}
+        {expandEducation &&
+        education.schools.map(school => (
+          <School key={school.id}
+            name={school.name}
+            dates={school.dates}
+            location={school.location}
+          />
+        ))}
+        {expandEducation &&
+          education.onlineCourses.map(course => (
+            <Course key={course.id}
+              name={course.name}
+              title={course.title}
+              dates={course.dates}
+              url={course.url}
+            />
+        ))}
+      </section>
+    );
+  }
+}
 
 // Presentational.
 const School = ({name, dates, location}) => (
@@ -111,26 +178,51 @@ const Course = ({name, title, dates, url}) => (
   </div>
 );
 
-const Projects = ({expandProjects, title}) => (
-  <section>
-    {!expandProjects &&
-    <h2
-      onClick={panel => store.dispatch(expand(projects))}
-    >Projects</h2>}
-    {expandProjects &&
-    projects.projects.map(project => (
-      <Project key={project.name}
-        // When a project is clicked, show it in an iframe.
-        onClick={pj => store.dispatch(show(project))}
-        project={project}
-        title={title}
-        name={project.name}
-        path={project.path}
-        src={project.src}
-      />
-    ))}
-  </section>
-);
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expandProjects: false,
+      title: ''
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState({
+      expandProjects: store.getState().expandProjects,
+      title: store.getState().title
+    }));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const expandProjects = this.state.expandProjects;
+    const title = this.state.title;
+    return (
+      <section>
+        {!expandProjects &&
+        <h2
+          onClick={panel => store.dispatch(expand(projects))}
+        >Projects</h2>}
+        {expandProjects &&
+        projects.projects.map(project => (
+          <Project key={project.name}
+            // When a project is clicked, show it in an iframe.
+            onClick={pj => store.dispatch(show(project))}
+            project={project}
+            title={title}
+            name={project.name}
+            path={project.path}
+            src={project.src}
+          />
+        ))}
+      </section>
+    );
+  }
+}
 
 const Project = ({project, title, name, path, src, onClick}) => (
   <div>
@@ -148,28 +240,50 @@ const Project = ({project, title, name, path, src, onClick}) => (
 
 // Presentational.
 // Might want to make a component for the paragraphs if they get too big.
-const Background = ({expandBackground}) => (
-  <section>
-    {!expandBackground &&
-    <h2
-      onClick={panel => store.dispatch(expand(background))}
-    >Background</h2>}
-    {expandBackground &&
-    <div>
-      <p>{background.firstP}</p>
-      <p>{background.secondP}</p>
-    </div>
-    }
-  </section>
-);
+class Background extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      expandBackground: false
+    };
+  }
+
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => this.setState({
+      expandBackground: store.getState().expandBackground
+    }));
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const expandBackground = this.state.expandBackground;
+    return (
+      <section>
+        {!expandBackground &&
+        <h2
+          onClick={panel => store.dispatch(expand(background))}
+        >Background</h2>}
+        {expandBackground &&
+        <div>
+          <p>{background.firstP}</p>
+          <p>{background.secondP}</p>
+        </div>
+        }
+      </section>
+    );
+  }
+}
 
 // Presentational (?)
-const Main = ({expandWork, expandEducation, expandProjects, title, expandBackground}) => (
+const Main = () => (
   <main>
-    <Work expandWork={expandWork} />
-    <Education expandEducation={expandEducation} />
-    <Projects expandProjects={expandProjects} title={title} />
-    <Background expandBackground={expandBackground} />
+    <Work />
+    <Education />
+    <Projects />
+    <Background />
   </main>
 );
 
@@ -184,20 +298,10 @@ const Footer = () => (
 // Takes in the Redux state as prop and pass it down.
 class App extends Component {
   render() {
-    const state = this.props.state;
     return (
       <div>
-        <Header
-          title={state.title}
-          description={state.description}
-        />
-        <Main
-          expandWork={state.expandWork}
-          expandEducation={state.expandEducation}
-          expandProjects={state.expandProjects}
-          title={state.title}
-          expandBackground={state.expandBackground}
-        />
+        <Header />
+        <Main />
         <Footer />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import store from './reducers';
 
-const render = () => ReactDOM.render(
-  // Pass the Redux state as prop to the React components.
-  <App state={store.getState()} />,
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
-
-// Subscribe the render() function to the store so it gets updated on change.
-// Also call it once to render the app the first time.
-store.subscribe(render);
-render();
